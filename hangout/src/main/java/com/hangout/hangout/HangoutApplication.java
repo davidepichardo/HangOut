2f70e39f3580a195d1d5c2fa8288ffe59d2fe83f
Xaiphon <dethanp13@gmail.com>
2026-04-03 11:33:47 -0700

    Basic Spring setup

---
+++ hangout/src/main/java/com/hangout/hangout/HangoutApplication.java
@@ -0,0 +1,13 @@
+package com.hangout.hangout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HangoutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HangoutApplication.class, args);
+	}
+
+}
---
+++ hangout/src/test/java/com/hangout/hangout/HangoutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hangout.hangout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HangoutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
